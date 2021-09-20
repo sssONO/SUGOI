9e155ba5e0104a49b3565fdc0c1d8d3e361e46b2
NOSHION <aserora411411@yahoo.co.jp>
2021-09-20 15:55:12 +0900

    初期

---
+++ src/components/TextList.vue
@@ -0,0 +1,14 @@
+<template>
+  <ul>
+    <li class="m-2" v-for="item in fuga" v-bind:key="item">言葉 : 「{{ item.text }}」 意味 : 「{{item.mean}}」</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "TextList",
+  props: { fuga : Array },
+};
+</script>
+
+<style></style>
---
+++ src/views/About.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="about">
+    <PageMove />
+  </div>
+</template>
+
+<script>
+import PageMove from "../components/PageMove.vue"
+
+export default {
+  name: "About",
+  components: {
+    PageMove,
+  },
+  data: () => ({
+   
+  }),
+  methods: {
+   
+  },
+}
+</script>
